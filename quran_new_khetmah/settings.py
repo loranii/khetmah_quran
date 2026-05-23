4af8857febb98f1ed78db21dc5ed6a5a1fc60078
Loranii <dr-loai@hotmail.com>
2026-05-24 00:34:39 +0200

    fix static duplicate paths

--- quran_new_khetmah/settings.py
+++ quran_new_khetmah/settings.py
@@ -124,7 +124,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -134,10 +133,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 # إعدادات الملفات الثابتة
-STATIC_URL = '/static/'
-
 STATICFILES_DIRS = [
-    BASE_DIR / 'khetmah/static',
+    os.path.join(BASE_DIR, 'khetmah/static'),
 ]
 
 
