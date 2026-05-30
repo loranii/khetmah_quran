48ab687aae2b20e30d2e7bdf87064f8f5a9de466
Loranii <dr-loai@hotmail.com>
2026-05-30 12:38:53 +0200

    restor the backup

--- quran_new_khetmah/urls.py
+++ quran_new_khetmah/urls.py
@@ -19,10 +19,8 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
-    path("control-panel-9274/", admin.site.urls),
+    path('admin/', admin.site.urls),
     path("", include("khetmah.urls")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
